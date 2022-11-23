51e2ba25fffe8cd648b715ebf43cfd7105350b30
Minjee Seo <79445201+Minjee-Seo@users.noreply.github.com>
2022-11-23 15:28:22 +0900

    Add files via upload

--- HRNet/lib/models/__init__.py
+++ HRNet/lib/models/__init__.py
@@ -8,5 +8,9 @@ from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
 
-from .mpii import MPIIDataset as mpii
-from .coco import COCODataset as coco
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+import models.pose_resnet
+import models.pose_hrnet
